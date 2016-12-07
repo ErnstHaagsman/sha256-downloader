f5a0bbdf6899a34fe7cd70d4367165ab616f03e8
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2016-12-07 11:12:13 +0100

    Usability improvements

--- download.py
+++ download.py
@@ -1,33 +1,52 @@
+import argparse
 import asyncio
 import hashlib
+from urllib.parse import urlsplit
+
 import aiohttp
 
 chunk_size = 1_024  # Set to 1 KB chunks
 
 
 async def download_url(url, destination):
+    print(f'Downloading {url}')
     file_hash = hashlib.sha256()
     with open(destination, 'wb') as file:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
+                total_bytes = int(response.headers['content-length'])
+                received = 0
                 while True:
                     chunk = await response.content.read(chunk_size)
                     if not chunk:
                         break
+                    received += chunk_size
+                    progress(received, total_bytes)
                     file_hash.update(chunk)
                     file.write(chunk)
 
-    print(f'Downloaded {destination}, sha256: {file_hash.hexdigest()}')
+    print(f'\r\nDownloaded {destination}, sha256: {file_hash.hexdigest()}')
+
+
+def progress(downloaded, total):
+    work_done = downloaded / total
+    # inspired by: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(work_done * 50), work_done * 100), end="", flush=True)
 
 
 def main():
-    # Let's download the newest version of PyCharm (community for mac because it's smallest...
-    # SHA256: 51f8d3e3f28c8d72c2eb33b28652b641e7f7a50ef3e054341dac5c64d4d7f792
+    # get the URL from the command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', metavar='URL', help='The URL to download')
+    arguments = parser.parse_args()
 
+    # get the filename from the URL
+    url_parts = urlsplit(arguments.url)
+    file_name = url_parts.path[url_parts.path.rfind('/') + 1:]
+
+    # start the download async
     loop = asyncio.get_event_loop()
-    print('Starting loop')
-    loop.run_until_complete(download_url('https://download.jetbrains.com/python/pycharm-community-2016.3.dmg',
-                                         'python-ce.dmg'))
+    loop.run_until_complete(download_url(arguments.url, file_name))
 
 
 if __name__ == '__main__':
